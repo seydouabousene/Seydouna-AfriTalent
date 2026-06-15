fbee17aead5449b0a010871453d9128acbecc720
seydou abou sene <seydouabousene@gmail.com>
2026-06-15 02:05:35 +0000

    HTML : structure complète des 5 pages avec sections et contenu texte

--- js/main.js
+++ js/main.js
@@ -57,3 +57,83 @@ backToTop.addEventListener('click', function() {
     behavior: 'smooth' // défilement fluide
   });
 });
+
+// commit7_____________________________________________________________________________________________________________________
+// PARTIE 1 — FADE IN
+
+const sections = document.querySelectorAll('.fade-section');
+// cree un observateur pour surveillez les section
+const surveillant = new IntersectionObserver(function(entries) {
+  entries.forEach(function(entry) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+// je lui demande quoi surveiller
+sections.forEach(function(section) {
+    // surville cette section
+  surveillant.observe(section); 
+
+});
+
+// PARTIE 2 — COMPTEUR SIMPLE
+
+function compter(id, but) {
+  const span = document.getElementById(id);
+  if (!span) return; // si le span n'existe pas → stop
+
+  let chiffre = 0;
+  const vitesse = but / 100;
+//   on divise le total en 100petit pat 
+
+  const intervalle = setInterval(function() {
+    // repete cette action
+    chiffre += vitesse;
+    // ajoute un pas 
+    span.textContent = Math.round(chiffre);
+    // affiche le chiffre darkTogglens le span 
+
+    if (chiffre >= but) {
+      span.textContent = but.toLocaleString();
+           // Quand on arrive au but → affiche le vrai chiffre
+      clearInterval(intervalle);
+    }
+  }, 20);
+}
+
+
+// PARTIE 3 — DÉCLENCHE AU SCROLL
+// pour surveillez les section stats
+const sectionStats = document.querySelector('.cole');
+const surveillantStats = new IntersectionObserver(function(entries) {
+  if (entries[0].isIntersecting) {
+
+    // Lance les compteurs index.html
+    compter('count-freelances', 2500);
+    compter('count-entreprises', 800);
+    compter('count-missions', 12000);
+    // arrete de surveiller
+    surveillantStats.unobserve(entries[0].target);
+  }
+});
+
+if (sectionStats) surveillantStats.observe(sectionStats);
+
+// MÊME CHOSE POUR ABOUT.HTML
+
+const sectionChiffres = document.querySelector('.cont');
+
+const surveillantChiffres = new IntersectionObserver(function(entries) {
+  if (entries[0].isIntersecting) {
+
+    compter('count-users', 48000);
+    compter('count-years', 7);
+    compter('count-projets', 320);
+    compter('count-satisfaits', 98);
+
+    surveillantChiffres.unobserve(entries[0].target);
+  }
+});
+
+if (sectionChiffres) surveillantChiffres.observe(sectionChiffres);
